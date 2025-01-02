b8328ae43ffa160a7570b123d72fd92ccfa4de74
Dmytro Polievoy <limondik601@gmail.com>
2025-01-02 17:56:59 +0200

    integration test - capture the stone

--- supabase/functions/calculate-captures-test/index.ts
+++ supabase/functions/calculate-captures-test/index.ts
@@ -4,20 +4,18 @@
 
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import { captureGroupInTheField } from "../tests/captureGroupInTheField";
+import { captureGroupInTheField } from "../tests/captureGroupInTheField.ts";
 
 Deno.serve(async (req) => {
   const results = [];
 
-  //ToDo:
-  //1. Run test 
-  captureGroupInTheField();
-  //2. Write the result of the test in results array as 
-  //    {testName: "captureGroupInTheField", result: true}
+  const captureResult = await captureGroupInTheField();
+
+  results.push({ testName: "captureGroupInTheField", result: captureResult});
 
   return new Response(
     JSON.stringify(results),
     { headers: { "Content-Type": "application/json" } },
-  )
+  );
 })
 
